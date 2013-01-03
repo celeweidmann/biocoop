26d26dbd2c9bacd00aaa17ae22ca365df0591861
cele <celeweidmann@gmail.com>
2013-01-03 00:31:18 -0300

    top principal y version 1. Contiene el menú principal, identico para todas las páginas.

---
+++ top/top.php
@@ -0,0 +1,3 @@
+<?php
+    include ("top/top1.php");
+?>
---
+++ top/top1.php
@@ -0,0 +1,41 @@
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+               <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li class="active"><a  class="brand" href="index.php">Biocoop</a></li>
+                            <li><a href="historia.php">Historia</a></li>
+                             <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Socios <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="socios.php">Lista de Socios</a></li>
+                                    <li><a href="consejo.php">Consejo de Administración</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#contact">Servicios</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Calidad <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="controlCalidad.php">Control de calidad</a></li>
+                                    <li><a href="gestionCalidad.php">Gestión de calidad</a></li>
+                                    <li class="divider"></li>
+                                    <li class="nav-header">ISO 9001:2008</li>
+                                    <li><a href="#">Certificado</a></li>
+                                </ul>
+                            </li>
+                             <li><a href="#contact">Contacto</a></li>
+                        </ul>
+                        <form class="navbar-form pull-right">
+                            <input class="span2" type="text" placeholder="Email">
+                            <input class="span2" type="password" placeholder="Password">
+                            <button type="submit" class="btn">Sign in</button>
+                        </form>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
+        </div>
